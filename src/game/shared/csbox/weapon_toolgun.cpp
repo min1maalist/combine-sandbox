ee4432019681dbabc09744cd3205e4af838015b3
GuestSneezeOSDev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-11-07 15:17:34 +0400

    a
    Co-Authored-By: ThePixelMoon <yourlocalmoon7@gmail.com>

---
+++ src/game/shared/csbox/weapon_toolgun.cpp
@@ -0,0 +1,9 @@
+//========= Copyright � 2009-20xx, Combine Sandbox, All rights reserved. ============//
+//
+// Purpose: 
+//
+// Toolgun Graphical User Interface Edition
+//=============================================================================//
+
+#include "cbase.h"
+// TODO
