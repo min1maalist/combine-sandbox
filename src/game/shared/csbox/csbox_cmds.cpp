b276be879d8d3e7f251df92ce40fbfa822ac1520
ThePixelMoon <yourlocalmoon7@gmail.com>
2024-11-17 19:20:22 +0200

    Update csbox_cmds.cpp

--- src/game/shared/csbox/csbox_cmds.cpp
+++ src/game/shared/csbox/csbox_cmds.cpp
@@ -1,8 +1,7 @@
 //========= Copyright � 2009-20xx, Combine Sandbox, All rights reserved. ============//
 //
-// Purpose: 
+// Purpose: For my Linux Homies(tm)
 //
-// For my Linux Homies
 //=============================================================================//
 
 #include "cbase.h"
@@ -18,7 +17,7 @@ const tchar* GetProcessorArchName()
 #elif defined __arm__ || defined _M_ARM
     return "arm";
 #else
-    return "Unknown architecture (are you 256 bit?)";
+    return "Unknown architecture";
 #endif
 }
 
@@ -39,70 +38,64 @@ const tchar* GetPlatformName()
 #elif PLATFORM_HAIKU
     return "Haiku";
 #else
-    return "Unknown Platform (what the fuck are you running this on?)"
-#endif
-}
-
-const tchar* GetGame()
-{
-#if defined( HL2_CLIENT_DLL )
-    return "Combine Sandbox";
-#elif defined(PORTAL_CLIENT_DLL)
-    return "Combine Sandbox";
-#elif defined(HL1_CLIENT_DLL)
-    return "Combine Sandbox (HL1_Base)";
-#elif defined(HL2MP)
-    return "Combine Sandbox (HL2DM_Base)";
-#elif defined(CSBOX)
-    return "Combine Sandbox";
-#else
-    return "Unknown Version (what the fuck?)";
+    return "Unknown Platform"
 #endif
 }
 
 void csboxfetch(void)
 {
+	Color orange( 128, 0, 128, 255 ); // L, not orange
 
-
-    Warning(" .-*#%@@@@@@@@@@@@@@@@@@@@@@@@@@@@%+.\n");
-    Warning(".=%@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*.\n");
-    Warning(" @@@@@@@#+:   ..-*@@@@@@@@@@@@@@@@@@@@@@@@*\n");
-    Warning(" -#%+:              .-*@@@@@@@@@@@@@@@@@@@@\n");
-    Warning("                       -%@@@@@@@@@@@@@@@@%\n");
-    Warning("       .....           .-@@@@@@@@@@@@@@@-\n");
-    Msg("  :*@@@@@@@@@@@*-"); Warning("   .#@@@@@@@@@@@@ + .\n");
-    Msg(".%@@@@@@@@@@@@@@@@."); Warning("       .#@@@@@@@@@@#\n");
-    Msg(".+@@@@@-     :%@@@@#."); Warning("     .#@@@@@@@@#.\n");
-    Msg(".*@@@@@*..    :-----."); Warning("       :@@@@@@@@:\n");
-    Msg("=@@@@@@@@@@%=.");        Warning("     .#@@@@@@=\n");
-    Msg(".+@@@@@@@@@@@@@%+.");    Warning("     .*@@@@@+\n");
-    Msg("     ..:*%%@@@@@@@@@:"); Warning("       .*@@@@#.\n");
-    Msg("           ..:#@@@@@*"); Warning("       .#@@@@:\n");
-    Msg(":%@@@@*.      =@@@@@+"); Warning("       -@@@@-\n");
-    Msg(".=@@@@@@#:..-%@@@@@%:"); Warning("      .@@@@=\n");
-    Msg(":#@@@@@@@@@@@@@@+.");    Warning("   .%@@@*.\n");
-    Msg("     :+##%%%%#*=.");     Warning("    : % @@@ % .\n");
-    Warning("                        .*@@@@*.\n");
-    Warning("                        -@@@@@-.\n");
-    Warning("                      .%@@+.\n");
-    Msg("Engine Version: Source 2013 (Zenith)\n");
+	ConColorMsg( orange, "        @@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" );
+	ConColorMsg( orange, "  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" );
+	ConColorMsg( orange, "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" );
+	ConColorMsg( orange, "@@@@@@@        @@@@@@@@@@@@@@@@@@@@@@@@\n" );
+	ConColorMsg( orange, " @@                @@@@@@@@@@@@@@@@@@@@\n" );
+	ConColorMsg( orange, "                      @@@@@@@@@@@@@@@@@\n" );
+	Msg( "   @@@@@@@@@@@@@" );
+	ConColorMsg( orange, "        @@@@@@@@@@@@@@ \n" );
+	Msg( "  @@@@@@@@@@@@@@@@" );
+	ConColorMsg( orange, "       @@@@@@@@@@@@@ \n" );
+	Msg( " @@@@@@@   @@@@@@@@" );
+	ConColorMsg( orange, "       @@@@@@@@@@@  \n" );
+	Msg( " @@@@@@@     @@@@@@" );
+	ConColorMsg( orange, "        @@@@@@@@@   \n" );
+	Msg( " @@@@@@@@@@@@@@@" );
+	ConColorMsg( orange, "           @@@@@@@@    \n" );
+	Msg( "  @@@@@@@@@@@@@@@@" );
+	ConColorMsg( orange, "          @@@@@@     \n" );
+	Msg( "     @@@@@@@@@@@@@@" );
+	ConColorMsg( orange, "         @@@@@@     \n" );
+	Msg( "@@@@@@      @@@@@@@@" );
+	ConColorMsg( orange, "       @@@@@@      \n" );
+	Msg( " @@@@@@      @@@@@@" );
+	ConColorMsg( orange, "        @@@@@       \n" );
+	Msg( " @@@@@@@@@@@@@@@@@@" );
+	ConColorMsg( orange, "       @@@@@        \n" );
+	Msg( "   @@@@@@@@@@@@@@" );
+	ConColorMsg( orange, "        @@@@@@        \n" );
+	Msg( "       @@@@@@" );
+	ConColorMsg( orange, "           @@@@@@         \n" );
+	ConColorMsg( orange, "                      @@@@@@           \n" );
+	ConColorMsg( orange, "                     @@@@@@            \n" );
+	ConColorMsg( orange, "                       @@@             \n\n" );
+    Msg("Engine: Source (Zenith)\n");
     Msg("Platform: %s\n", GetPlatformName());
-    Msg("Arch: %s\n", GetProcessorArchName());
-    Msg("Game: %s\n", GetGame());
+    Msg("Architecture: %s\n", GetProcessorArchName());
+    Msg("Game: Combine Sandbox\n");
 }
 
 ConCommand neofetch("neofetch", csboxfetch, "");
 
-CON_COMMAND(friendlyfire, "Friendly Fire for Combine Sandbox") {
+CON_COMMAND(friendlyfire, "Friendly Fire") {
     engine->ClientCmd("ent_create ai_relationship subject !player target * disposition 1 rank 99 reciprocal 0 startactive 1");
 }
 
 CON_COMMAND(toolgun, "Toolgun Utils") {
-    Msg("TOOLGUN - COMBINE SANDBOX EDITION\n");
     engine->ClientCmd("exec toolgun");
 }
 
-CON_COMMAND(singleplayer, "Play Combine Sandbox in Singleplayer") {
+CON_COMMAND(singleplayer, "Play Singleplayer") {
     engine->ClientCmd("maxplayers 1");
     engine->ClientCmd("sv_lan 1");
     engine->ClientCmd("sv_cheats 1");
@@ -113,7 +106,7 @@ CON_COMMAND(singleplayer, "Play Combine Sandbox in Singleplayer") {
 
 CON_COMMAND(shitify, "Destroy's the graphics") {
     engine->ClientCmd("sv_cheats 1; mat_dxlevel 60; cl_ragdoll_collide 0; mat_picmip 4; r_WaterDrawReflection 0; r_WaterDrawRefraction 0; mat_wateroverlaysize 8; r_lod 5; mat_fullbright 1; mat_diffuse 5; mat_bumpmap 0; r_shadows 0; mat_forceaniso 0; mat_forcehardwaresync 0; mat_reducefillrate 0; mat_mipmaptextures 0; mat_filtertextures 0; mat_bloom 0; mat_envmapsize 32; mat_envmapgasize 8; mat_antialiasing 0; cl_phys_props_enable 0; mat_filterlightmaps 0; mat_showlowresimage 1");
-    engine->ClientCmd("echo 'Shitified your graphics'");
+    Msg("Shitified your graphics successfuly");
 }
 
 CON_COMMAND(spawnmenu, "Spawn Menu") {
