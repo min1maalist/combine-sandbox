7bed40fee237a669f589675070d9f31035a90479
GuestSneezeOSDev <163439609+GuestSneezeOSDev@users.noreply.github.com>
2024-11-27 12:50:52 +0400

    themaster974 is goated fr fr

--- game/csbox/lua/weapons/weapon_ak47/cl_init.lua
+++ game/csbox/lua/weapons/weapon_ak47/cl_init.lua
@@ -1,4 +1,4 @@
---========== Copyleft © 2010, Team Sandbox, Some rights reserved. ===========--
+--========== Copyleft ï¿½ 2010, Team Sandbox, Some rights reserved. ===========--
 --
 -- Purpose: Initialize the base scripted weapon.
 --
